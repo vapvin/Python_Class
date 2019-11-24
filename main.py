8cad15b26af458a80164eb98a87b9b744b21a2bc
vapvin <tpehdj12@daum.net>
2019-11-24 13:30:16 +0900

    Abstarct class

--- main.py
+++ main.py
@@ -57,3 +57,23 @@ class Language:
 
 class KoreanLanguage(Language):
     default_language = "한국어"
+
+
+
+class AbstractCountry(metaclass=ABCMeta):
+    name = '국가명'
+    population = '인구'
+    capital = '수도'
+
+    def show(self):
+        print('국가 클래스의 메소드입니다.')
+
+class Korea(AbstractCountry):
+
+    def __init__(self, name,population, capital):
+        self.name = name
+        self.population = population
+        self.capital = capital
+
+    def show_name(self):
+        print('국가 이름은 : ', self.name)
