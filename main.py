1e3de704cb737174302a4ae492753a830fb6012b
vapvin <tpehdj12@daum.net>
2019-11-24 13:29:29 +0900

    Classmethod or staticmethod

--- main.py
+++ main.py
@@ -34,3 +34,26 @@ class CustomClass:
     @staticmethod
     def add_static_method(a, b):
         return a + b
+
+
+
+class Language:
+    default_language = "English"
+
+    def __init__(self):
+        self.show = '나의 언어는' + self.default_language
+
+    @classmethod
+    def class_my_language(cls):
+        return cls()
+
+    @staticmethod
+    def static_my_language():
+        return Language()
+
+    def print_language(self):
+        print(self.show)
+
+
+class KoreanLanguage(Language):
+    default_language = "한국어"
