b8aa787dcb64de6cb78cfd61c8e42728130056c8
vapvin <tpehdj12@daum.net>
2019-11-25 21:33:56 +0900

    Vars

--- main.py
+++ main.py
@@ -370,3 +370,19 @@ if __name__ == '__main__':
         print(var2)
 
     print("Good bye!")
+
+    var = 100
+    if var == 200:
+        print("1 - Got a true expression value")
+        print(var)
+    elif var == 150:
+        print("2 - Got a true expression value")
+        print(var)
+    elif var == 100:
+        print("3 - Got a true expression value")
+        print(var)
+    else:
+        print("4 - Got a false expression value")
+        print(var)
+
+    print("Good bye!")
