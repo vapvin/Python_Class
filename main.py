5f9e18f4b61a395173dda89964c1d4df67f55048
vapvin <tpehdj12@daum.net>
2019-11-25 21:33:21 +0900

    If and Else

--- main.py
+++ main.py
@@ -352,3 +352,21 @@ class MyUtilTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+    var1 = 100
+    if var1:
+        print("1 - Got a true expression value")
+        print(var1)
+    else:
+        print("1 - Got a false expression value")
+        print(var1)
+
+    var2 = 0
+    if var2:
+        print("2 - Got a true expression value")
+        print(var2)
+    else:
+        print("2 - Got a false expression value")
+        print(var2)
+
+    print("Good bye!")
