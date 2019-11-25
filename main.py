11cc0d381266f3d350600dc21335f277551dbd43
vapvin <tpehdj12@daum.net>
2019-11-25 21:30:44 +0900

    Pushing python study

--- main.py
+++ main.py
@@ -280,3 +280,23 @@ def add(a, b):
 
 def substract(a, b):
     return a - b
+
+
+# tests.py
+import unittest
+import myCalc
+
+
+class MyCalcTest(unittest.TestCase):
+
+    def test_add(self):
+        c = myCalc.add(20, 10)
+        self.assertEqual(c, 30)
+
+    def test_substract(self):
+        c = myCalc.substract(20, 10)
+        self.assertEqual(c, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
