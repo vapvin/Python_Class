859a710d325e1c18b3fda0acc741be0ce3cbb829
vapvin <tpehdj12@daum.net>
2019-11-25 21:31:17 +0900

    Study Python To day is 1125

--- main.py
+++ main.py
@@ -300,3 +300,22 @@ class MyCalcTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+# myUtil.py
+import os
+
+
+def filelen(filename):
+    f = open(filename, "r")
+    f.seek(0, os.SEEK_END)
+    return f.tell()
+
+
+def count_in_file(filename, char_to_find):
+    count = 0
+    f = open(filename, "r")
+    for word in f:
+        for char in word:
+            if char == char_to_find:
+                count += 1
+    return count
