b432a5a8ebd31d592d8e1c32d39c41f3d3a681aa
vapvin <tpehdj12@daum.net>
2019-11-25 21:28:57 +0900

    Df. classes

--- main.py
+++ main.py
@@ -246,6 +246,26 @@ print(Rectangle.count)
 print(r.count)
 
 
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def move(self):
+        print("move")
+
+    def speak(self):
+        pass
+
+
+class Dog(Animal):
+    def speak(self):
+        print("bark")
+
+
+class Duck(Animal):
+    def speak(self):
+        print("quack")
+
 import math
 
 n = math.sqrt(9.0)
