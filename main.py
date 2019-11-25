03e0be849d1d94923310fd67fb612cadd7e3a55e
vapvin <tpehdj12@daum.net>
2019-11-25 21:27:39 +0900

    Class is too Hard

--- main.py
+++ main.py
@@ -196,6 +196,41 @@ class Rectangle:
         return area
 
 
+class Rectangle:
+    count = 0  # 클래스 변수
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        Rectangle.count += 1
+
+    # 인스턴스 메서드
+    def calcArea(self):
+        area = self.width * self.height
+        return area
+
+    # 정적 메서드
+    @staticmethod
+    def isSquare(rectWidth, rectHeight):
+        return rectWidth == rectHeight
+
+        # 클래스 메서드
+
+    @classmethod
+    def printCount(cls):
+        print(cls.count)
+
+    # 테스트
+
+
+square = Rectangle.isSquare(5, 5)
+print(square)  # True
+
+rect1 = Rectangle(5, 5)
+rect2 = Rectangle(2, 5)
+rect1.printCount()  # 2
+
+
 import math
 
 n = math.sqrt(9.0)
