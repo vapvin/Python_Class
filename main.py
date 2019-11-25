f23b4639b66ea9ec4e6bbc3c80a11b173ec8bae7
vapvin <tpehdj12@daum.net>
2019-11-25 21:31:58 +0900

    This Main.py are Study Python with Web

--- main.py
+++ main.py
@@ -319,3 +319,36 @@ def count_in_file(filename, char_to_find):
             if char == char_to_find:
                 count += 1
     return count
+
+
+import unittest
+import os
+import myUtil
+
+
+class MyUtilTest(unittest.TestCase):
+    testfile = 'test.txt'
+
+    # Fixture
+    def setUp(self):
+        f = open(MyUtilTest.testfile, 'w')
+        f.write('1234567890')
+        f.close()
+
+    def tearDown(self):
+        try:
+            os.remove(MyUtilTest.testfile)
+        except:
+            pass
+
+    def test_filelen(self):
+        leng = myUtil.filelen(MyUtilTest.testfile)
+        self.assertEqual(leng, 10)
+
+    def test_count_in_file(self):
+        cnt = myUtil.count_in_file(MyUtilTest.testfile, '0')
+        self.assertEqual(cnt, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
